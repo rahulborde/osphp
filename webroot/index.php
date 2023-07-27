2a1da55ee99537968f2a92bc77a8b47f48cb57ff
rahulborde <borde1111@gmail.com>
2023-07-27 15:25:18 +0530

    Update index.php

--- webroot/index.php
+++ webroot/index.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The Front Controller for handling every request
+ * TThe Front Controller for handling every request
  *
  * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
  * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
